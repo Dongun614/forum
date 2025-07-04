feea19367c5251590435ed5695dc1ff9357c5898
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2025-07-04 15:30:15 +0900

    feat: DB 연결

--- app/page.js
+++ app/page.js
@@ -1,5 +1,14 @@
-export default function Home() {
+import { connectDB } from "@/util/database"
+import { MongoClient } from "mongodb"
+
+export default async function Home() {
+  
+  const client = await connectDB;
+  const db = client.db("forum")
+  let result = await db.collection('post').find().toArray()
+  console.log(result)
+
   return (
-    <div>안녕</div>
+    <div>안녕</div> 
   )
 }
