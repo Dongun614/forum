5aecdcbb15e6bcdc43169b323dfd8c2a296de43e
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2025-07-06 17:54:28 +0900

    Feat: 삭제 기능 추가

--- app/list/ListItem.js
+++ app/list/ListItem.js
@@ -3,16 +3,24 @@
 import Link from "next/link"
 import { useEffect } from "react"
 
-export default function ListItem(props){
-    
+export default function ListItem(props) {
+
     return (
         <div>
             {
                 props.result.map((a, i) => {
                     return (
-                        <div className="list-item" key = { i }>
+                        <div className="list-item" key={i}>
                             <Link href={`detail/${props.result[i]._id}`}>{props.result[i].title}</Link>
                             <Link href={`/edit/${props.result[i]._id}`}>✏️</Link>
+                            <button onClick={() => {
+                                console.log(props.result[i]._id)
+                                fetch('/api/post/delete', { method : 'DELETE', body : props.result[i]._id})
+                                    .then((r)=> r.json())
+                                    .then((result)=>{
+                                        //성공시 실행할 코드
+                                    })
+                            }}>🗑️</button>
                             <p>{props.result[i].content}</p>
                         </div>
                     )
