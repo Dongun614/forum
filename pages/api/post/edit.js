573636ebacf3f6f6ee87b194c950b848470af145
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2025-07-06 10:53:58 +0900

    feat: 수정기능

--- pages/api/post/edit.js
+++ pages/api/post/edit.js
@@ -1,19 +1,17 @@
-import { connectDB } from "@/util/database"
+import { connectDB } from "@/util/database";
 import { ObjectId } from "mongodb";
 
 export default async function handler(req, res){
-    let client = await connectDB;
-    let db = client.db("forum")
-    let find = await db.collection("post").findOne({
-            _id : new ObjectId(props.params.id)
-        })
+    if(req.method == 'POST'){
+        let 바꿀거 = { title : req.body.title , content : req.body.content}
+        const db = (await connectDB).db("forum")
+        let result = await db.collection('post').updateOne(
+            {_id : new ObjectId(req.body._id)}, 
+            {$set : 바꿀거}
+        )
 
-    console.log(find)
+        console.log(result)
+    }
 
-    return res.status(200).json("수정 완료")
+    return res.status(200).redirect('/list')
 }
-    
-    // await db.collection("post").updateOne(
-    //     {수정할게시물정보}, 
-    //     {$set: { title : '바보' }}
-    // )
